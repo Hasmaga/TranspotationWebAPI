bfa1169de612c328a12484b825dafe3a3f7ab19d
Bui Pham <chilythuongtin123@gmail.com>
2022-09-26 05:42:40 +0700

    Migrations

---
+++ TranspotationWebAPI/Migrations/20220925224122_Initial.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TranspotationWebAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Car",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarType",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeCar = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Company",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Company", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Location",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Location", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Authority = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Station",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Station", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Station_Location_LocationId",
+                        column: x => x.LocationId,
+                        principalSchema: "dbo",
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trip",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    From_Id = table.Column<int>(type: "int", nullable: true),
+                    To_Id = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trip", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trip_Location_From_Id",
+                        column: x => x.From_Id,
+                        principalSchema: "dbo",
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Trip_Location_To_Id",
+                        column: x => x.To_Id,
+                        principalSchema: "dbo",
+                        principalTable: "Location",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: true),
+                    RoleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalSchema: "dbo",
+                        principalTable: "Company",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Account_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "dbo",
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyTrip",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TripId = table.Column<int>(type: "int", nullable: true),
+                    CarId = table.Column<int>(type: "int", nullable: true),
+                    CompanyId = table.Column<int>(type: "int", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: true),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: true),
+                    CarTypeId = table.Column<int>(type: "int", nullable: true),
+                    StationId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyTrip", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompanyTrip_Car_CarId",
+                        column: x => x.CarId,
+                        principalSchema: "dbo",
+                        principalTable: "Car",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CompanyTrip_CarType_CarTypeId",
+                        column: x => x.CarTypeId,
+                        principalSchema: "dbo",
+                        principalTable: "CarType",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CompanyTrip_Company_CompanyId",
+                        column: x => x.CompanyId,
+                        principalSchema: "dbo",
+                        principalTable: "Company",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CompanyTrip_Station_StationId",
+                        column: x => x.StationId,
+                        principalSchema: "dbo",
+                        principalTable: "Station",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CompanyTrip_Trip_TripId",
+                        column: x => x.TripId,
+                        principalSchema: "dbo",
+                        principalTable: "Trip",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Total = table.Column<int>(type: "int", nullable: true),
+                    AccountId = table.Column<int>(type: "int", nullable: true),
+                    CompanyTripId = table.Column<int>(type: "int", nullable: true),
+                    SeatName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ticket_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalSchema: "dbo",
+                        principalTable: "Account",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Ticket_CompanyTrip_CompanyTripId",
+                        column: x => x.CompanyTripId,
+                        principalSchema: "dbo",
+                        principalTable: "CompanyTrip",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_CompanyId",
+                schema: "dbo",
+                table: "Account",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_RoleId",
+                schema: "dbo",
+                table: "Account",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyTrip_CarId",
+                schema: "dbo",
+                table: "CompanyTrip",
+                column: "CarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyTrip_CarTypeId",
+                schema: "dbo",
+                table: "CompanyTrip",
+                column: "CarTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyTrip_CompanyId",
+                schema: "dbo",
+                table: "CompanyTrip",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyTrip_StationId",
+                schema: "dbo",
+                table: "CompanyTrip",
+                column: "StationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyTrip_TripId",
+                schema: "dbo",
+                table: "CompanyTrip",
+                column: "TripId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Station_LocationId",
+                schema: "dbo",
+                table: "Station",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_AccountId",
+                schema: "dbo",
+                table: "Ticket",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_CompanyTripId",
+                schema: "dbo",
+                table: "Ticket",
+                column: "CompanyTripId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trip_From_Id",
+                schema: "dbo",
+                table: "Trip",
+                column: "From_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trip_To_Id",
+                schema: "dbo",
+                table: "Trip",
+                column: "To_Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticket",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Account",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "CompanyTrip",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Role",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Car",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "CarType",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Company",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Station",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Trip",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Location",
+                schema: "dbo");
+        }
+    }
+}
